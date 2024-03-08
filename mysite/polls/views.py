d3ea2ad3a1d115a708d95f0113dc2617785c8557
Leonardo Viana <leovir957@gmail.com>
2024-03-08 17:18:52 +0000

    atividade 1 dsweb - 18/mar

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request ):
-    return HttpResponse('Pools app DSWeb 2024.1')
+    return HttpResponse('<h1>Desenvolvimento de Sistemas WEB</h1><h2>3o Período</h2><h3>20231014040010</h3><h3>Leonardo Viana Brito</h3>')
