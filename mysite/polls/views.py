9424a0703b5c06300f565c1a23d9b33933357361
Leonardo Viana <leovir957@gmail.com>
2024-03-18 14:13:48 +0000

    aula 5

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,14 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+
+from .models import Question
 
 def index(request ):
-    return HttpResponse('<h1>Desenvolvimento de Sistemas WEB</h1><h2>3o Período</h2><h3>20231014040010</h3><h3>Leonardo Viana Brito</h3>')
-
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+    ##return HttpResponse('<h1>Desenvolvimento de Sistemas WEB</h1><h2>3o Período</h2><h3>20231014040010</h3><h3>Leonardo Viana Brito</h3>')
 
 ##Adiantando aula views e templates
 def detail(request, question_id):
-    response = "Voce esta visualizando a questao %s."
-    return HttpResponse(response % question_id)
+
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html',{'question':question})
 
 def results(request, question_id):
     response = "Resultados da questao %s."
